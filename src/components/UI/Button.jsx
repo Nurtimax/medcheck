fb2ef0f205d6ccf7e1555de96faa44b279cd3893
Asylbek <asylbek310800@gmail.com>
2023-01-05 18:22:07 +0600

    button

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -22,24 +22,27 @@ const ButtonStyle = styled(MuiButton)`
    font-weight: 500;
    font-size: 14px;
    line-height: 18px;
-   border-radius: 6px;
+   border-radius: 10px;
    color: #fff4f4;
    padding: 10px 12px;
    cursor: 'pointer';
    text-align: center;
 
    &.MuiButton-outlined {
-      color: ${({ bgColor }) => bgColor || '#43ae28'};
+      color: #048741;
+      color: ${({ bgColor }) =>
+         bgColor ||
+         'linear-gradient(180.61deg, #0CBB6B 0.45%, #027B44 99.39%)'};
       border-color: ${({ bgColor }) => bgColor || '#43ae28'};
    }
    &:hover {
-      background: ${(props) => props.bgColor || '#3ebb1f'};
+      background: ${(props) => props.bgColor || '#15D14A'};
       color: #ffffff;
       transition-duration: '0.6s';
       border: 'none';
    }
    &:active {
-      background: ${({ bgColor }) => bgColor || '#43ae28'};
+      background: ${({ bgColor }) => bgColor || '#0CA336'};
       box-shadow: 'rgba(225, 228, 232, 0.2) 0 1px 0 inset';
    }
    &:disabled {
@@ -47,16 +50,15 @@ const ButtonStyle = styled(MuiButton)`
       color: #d0bebe;
       box-shadow: none;
       cursor: 'default';
-      border: 'none';
+      border-color: white;
    }
    &.MuiButton-contained {
-      background: ${({ bgColor }) => bgColor || '#43ae28'};
+      background: ${({ bgColor }) => bgColor || '#13C645'};
       font-size: 14px;
       border: none;
       color: #ffffff;
-
       &:hover {
-         background: ${({ bgColor }) => bgColor || '#3ebb1f'};
+         background: ${({ bgColor }) => bgColor || ' #0FAE3B'};
          color: #ffffff;
          transition-duration: '0.6s';
          border: 'none';
