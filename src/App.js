f06702079987470912b057bc7f1d96be6b8add44
Kamchybekk <manasbekoovich@gmail.com>
2023-01-09 17:38:47 +0600

    clear app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import Button from './components/UI/Button'
 function App() {
-   return (
-      <div>
-         <Button variant="outlined" width="200px">
-            ecvewvwe
-         </Button>
-         <Button disabled="disabled">ecvewvwe</Button>
-         <Button variant="contained">ecvewvwe</Button>
-         <Button disabled="disabled">ecvewvwe</Button>
-      </div>
-   )
+   return <div></div>
 }
 
 export default App
