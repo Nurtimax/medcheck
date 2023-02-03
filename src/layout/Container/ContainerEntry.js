cd9d8c5bb991a682dad236f0bc44af4d5409d3d1
kalmanbetova <samu21rales@gmail.com>
2023-02-03 09:41:04 +0300

    add style

--- src/layout/Container/ContainerEntry.js
+++ src/layout/Container/ContainerEntry.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import { styled } from '@mui/material'
-import { Container, Box } from '@mui/material'
+import { Container } from '@mui/material'
 
 function ContainerEntry({ children }) {
-   return (
-      <Box>
-         <MainContainer>{children}</MainContainer>
-      </Box>
-   )
+   return <MainContainer>{children}</MainContainer>
 }
 
 export default ContainerEntry
@@ -15,4 +11,5 @@ export default ContainerEntry
 const MainContainer = styled(Container)(() => ({
    background: '#FFFFFF',
    boxShadow: '0 0 20px black',
+   borderRadius: '6px',
 }))
