41671f738e1386bb01bdd2ecc9d3cdfef1fa51b3
kalmanbetova <samu21rales@gmail.com>
2023-02-13 12:39:46 +0300

    change

--- src/pages/AboutClinic.js
+++ src/pages/AboutClinic.js
@@ -8,6 +8,7 @@ import photoMedPersonal1 from '../assets/images/photoMedPersonal1.jpg'
 import photoMedPersonal2 from '../assets/images/photoMedPersonal2.jpg'
 import Button from '../components/UI/Button.jsx'
 import { useState } from 'react'
+import Contacts from '../contacts/Contacts'
 
 const AboutClinicPage = () => {
    const [show, setShow] = useState(false)
@@ -15,107 +16,123 @@ const AboutClinicPage = () => {
       setShow(true)
    }
    return (
-      <Container>
-         <StyledTitle>
-            <firstSpan>Главная</firstSpan> <secondSpan> О клинике</secondSpan>
-         </StyledTitle>
-         <StyledHeaderTitle>
-            <thirdSpan>Здоровье — самое </thirdSpan>
-            <fourthSpan>ценное в жизни</fourthSpan>
-         </StyledHeaderTitle>{' '}
-         <TopContainer>
-            <TextMain>
-               <p className="textSection">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum
-               </p>
-               <p className="textSection1">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum
-               </p>{' '}
-               <Picture>
-                  <img src={womenSignature} alt="women" />
-               </Picture>
-            </TextMain>
-            <div>
-               <PictureSection>
-                  <PictureBlock>
-                     <img
-                        className="imgManager"
-                        src={womenPhoto}
-                        alt="manager"
-                     />
-                     <p className="managerStyle">
-                        Руководитель клиники Medical Clinic
+      <>
+         {show ? (
+            <Contacts />
+         ) : (
+            <Container>
+               <StyledTitle>
+                  <firstSpan>Главная</firstSpan>{' '}
+                  <secondSpan> О клинике</secondSpan>
+               </StyledTitle>
+               <StyledHeaderTitle>
+                  <thirdSpan>Здоровье — самое </thirdSpan>
+                  <fourthSpan>ценное в жизни</fourthSpan>
+               </StyledHeaderTitle>{' '}
+               <TopContainer>
+                  <TextMain>
+                     <p className="textSection">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum
                      </p>
-                     <p className="nameStyle">Аниса Михаилова</p>
-                  </PictureBlock>
-               </PictureSection>
-            </div>
-         </TopContainer>
-         <BottomContainer>
-            <BlockTitle>
-               <span className="titleSpan">О нашей клинике</span>
-               <span className="titleSpan1">“MedCheck”</span>
-            </BlockTitle>
-            <StyledBottomMain>
-               <TextBottomMain>
-                  <p className="firstText">
-                     Вся наша команда готова обеспечить вам медицинский уход и
-                     заботу на самом высоком уровне. Наша главная задача —
-                     оказать Вам теплый прием и обеспечить самый лучший
-                     медицинский уход. У нас Вы в хороших руках! В нашей клинике
-                     используются только качественные материалы и проверенные
-                     технологии. Для каждого клиента специалисты нашей клиники
-                     разработают индивидуальный план лечения, подробно
-                     рассказывая о каждом этапе.
-                  </p>
-                  <p className="secondText">
-                     Доброжелательность и уважительное отношение к пациентам, не
-                     только материальная, но и моральная ответственность за
-                     результаты лечения — все это взято за основу политики
-                     Medical Clinic. Профессионализм и высокое качество
-                     оказываемых услуг помогают нам привлечь пациентов которые
-                     рекомендуют нас своим родным и близким.
-                  </p>
-                  <p className="thirdText">
-                     Уже 20 лет мы работаем на уровне лучших мировых стандартов,
-                     внедряя и развивая передовые методы лечения для сохранения
-                     здоровья наших пациентов
-                  </p>
+                     <p className="textSection1">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum
+                     </p>{' '}
+                     <Picture>
+                        <img src={womenSignature} alt="women" />
+                     </Picture>
+                  </TextMain>
+                  <div>
+                     <PictureSection>
+                        <PictureBlock>
+                           <img
+                              className="imgManager"
+                              src={womenPhoto}
+                              alt="manager"
+                           />
+                           <p className="managerStyle">
+                              Руководитель клиники Medical Clinic
+                           </p>
+                           <p className="nameStyle">Аниса Михаилова</p>
+                        </PictureBlock>
+                     </PictureSection>
+                  </div>
+               </TopContainer>
+               <BottomContainer>
+                  <BlockTitle>
+                     <span className="titleSpan">О нашей клинике</span>
+                     <span className="titleSpan1">“MedCheck”</span>
+                  </BlockTitle>
+                  <StyledBottomMain>
+                     <TextBottomMain>
+                        <p className="firstText">
+                           Вся наша команда готова обеспечить вам медицинский
+                           уход и заботу на самом высоком уровне. Наша главная
+                           задача — оказать Вам теплый прием и обеспечить самый
+                           лучший медицинский уход. У нас Вы в хороших руках! В
+                           нашей клинике используются только качественные
+                           материалы и проверенные технологии. Для каждого
+                           клиента специалисты нашей клиники разработают
+                           индивидуальный план лечения, подробно рассказывая о
+                           каждом этапе.
+                        </p>
+                        <p className="secondText">
+                           Доброжелательность и уважительное отношение к
+                           пациентам, не только материальная, но и моральная
+                           ответственность за результаты лечения — все это взято
+                           за основу политики Medical Clinic. Профессионализм и
+                           высокое качество оказываемых услуг помогают нам
+                           привлечь пациентов которые рекомендуют нас своим
+                           родным и близким.
+                        </p>
+                        <p className="thirdText">
+                           Уже 20 лет мы работаем на уровне лучших мировых
+                           стандартов, внедряя и развивая передовые методы
+                           лечения для сохранения здоровья наших пациентов
+                        </p>
 
-                  <Button
-                     onClick={navigateToComponent}
-                     variant="outlined"
-                     width="400px"
-                  >
-                     Записаться на консультацию
-                  </Button>
-                  {show && <h1>modal</h1>}
-               </TextBottomMain>
-               <PictureSection1>
-                  <img src={photoMedCenter} alt="photoMedCenter" />
-                  <PictureBottomSection>
-                     <img src={photoMedPersonal} alt="photoMedPersonal" />
-                     <img src={photoMedPersonal1} alt="photoMedPersonal1" />
-                     <img src={photoMedPersonal2} alt="photoMedPersonal2" />
-                  </PictureBottomSection>
-               </PictureSection1>
-            </StyledBottomMain>
-         </BottomContainer>
-      </Container>
+                        <Button
+                           onClick={navigateToComponent}
+                           variant="outlined"
+                           width="400px"
+                        >
+                           Записаться на консультацию
+                        </Button>
+                     </TextBottomMain>
+                     <PictureSection1>
+                        <img src={photoMedCenter} alt="photoMedCenter" />
+                        <PictureBottomSection>
+                           <img src={photoMedPersonal} alt="photoMedPersonal" />
+                           <img
+                              src={photoMedPersonal1}
+                              alt="photoMedPersonal1"
+                           />
+                           <img
+                              src={photoMedPersonal2}
+                              alt="photoMedPersonal2"
+                           />
+                        </PictureBottomSection>
+                     </PictureSection1>
+                  </StyledBottomMain>
+               </BottomContainer>
+            </Container>
+         )}
+      </>
    )
 }
 
