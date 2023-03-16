eb5f7b28549a079b94425acfd7d8ff4c8a6edba7
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-16 20:37:56 +0600

    change private route function

--- src/routes/private/Private.Route.js
+++ src/routes/private/Private.Route.js
@@ -1,11 +1,23 @@
+import { useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
 
-export default function ProtectedRoute({ element, roles }) {
-   const { admin } = useSelector((state) => state.auth.role)
+const PrivateRoute = ({ Component, role = [], fallbackPath }) => {
+   const { isAuth, roleName } = useSelector((state) => state.auth)
 
-   const isAdminHasRole = admin && roles.includes(admin)
+   const checkRole = useMemo(() => {
+      return role.join().toLowerCase().split().includes(roleName?.toLowerCase())
+   }, [roleName, role])
 
-   if (!isAdminHasRole) return <Navigate to="/" replace />
-   return element
+   if (!isAuth) {
+      return <Navigate to="/" replace />
+   }
+
+   if (isAuth && !checkRole) {
+      return <Navigate to={fallbackPath} replace />
+   }
+
+   return Component
 }
+
+export default PrivateRoute
